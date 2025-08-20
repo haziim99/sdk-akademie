bc39dc7caf48f83f80c3fe4fa703c99c7cab01d9
Hazem Mostafa <mustafahazem018@gmail.com>
2025-08-20 19:34:15 +0200

    Adjusting the proccess

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyC1RNKFIJDoZPa-pqIrifrvuNCTAm22lSA",
-    authDomain: "sdk-akademie1.firebaseapp.com",
-    projectId: "sdk-akademie1",
-    storageBucket: "sdk-akademie1.appspot.com",
-    messagingSenderId: "669541836589",
-    appId: "1:669541836589:web:61c73a553124a9d90e55a4",
-    measurementId: "G-R3BQ9VTS5X"
+    firebaseConfig: {
+        apiKey: "AIzaSyCL1zs7mVkugzizMFM-VDnSGpQkic1snWQ",
+        authDomain: "sdk-akademie1.firebaseapp.com",
+        projectId: "sdk-akademie1",
+        storageBucket: "sdk-akademie1.appspot.com",
+        messagingSenderId: "669541836589",
+        appId: "1:669541836589:web:61c73a553124a9d90e55a4",
+        measurementId: "G-R3BQ9VTS5X"
   },
   cloudinary: {
     cloudName: 'dzmcteb1t',
